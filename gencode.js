b046465236fb8fbd6ea4681f7881a6bde51d9650
Giang Nam <namle@kinssolutions.net>
2016-08-09 16:22:32 +0700

    asd

---
+++ gencode.js
@@ -0,0 +1,55 @@
+var fs = require("fs");
+var param = process.argv[2];
+
+var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+function randomString(length) {
+    var result = '';
+    for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+    return result;
+}
+
+if(param == "clear") {
+  fs.writeFile("promotion.code", "", function(err) {
+    if(err) {
+      console.log(err);
+    }
+  });
+}
+else {
+
+  var quantity = parseInt(param);
+
+  fs.readFile('promotion.code', 'utf8', function(err, data) {
+    if(err) {
+      console.log(err);
+      res.send(err);
+      return;
+    }
+    var data = data.replace(/\n+/g, " ").trim()
+    var codes = data == "" ? [] : data.split(' ');
+
+    function exists(c) {
+      for(var i=0; i<codes.length; i++)
+        if(codes[i] == c)
+          return true;
+      return false;
+    }
+
+    for(var i=0; i<quantity; i++) {
+      var newCode = randomString(9);
+      while(exists(newCode)) newCode = randomString(9);
+      console.log(newCode);
+      codes.push(newCode);
+    }
+
+    console.log("Total codes: " + codes.length);
+    codes = codes.join("\n");
+    fs.writeFile("promotion.code", codes, function(err) {
+      if(err) {
+        console.log(err);
+      }
+    });
+
+  });
+
+}
--- index.js
+++ index.js
@@ -10,8 +10,41 @@ app.get('/sale', function (req, res) {
   res.send('1');
 });
 
+app.get('/redeem/:code', function (req, res) {
+  var code = req.params["code"];
+
+  fs.readFile('promotion.code', 'utf8', function(err, data) {
+    if(err) {
+      console.log(err);
+      res.send(err);
+      return;
+    }
+
+    var data = data.replace(/\n+/g, " ").trim()
+    var codes = data == "" ? [] : data.split(' ');
+    console.log("Total codes: " + codes.length);
+
+    for(var i=0; i<codes.length; i++)
+      if(code == codes[i]) {
+        res.send("1");
+        codes.splice(i,1);
+        codes = codes.join("\n");
+
+        fs.writeFile("promotion.code", codes, function(err) {
+          if(err) {
+            console.log(err);
+          }
+        });
+
+        return;
+      }
+    res.send("0");
+  });
+
+});
+
 app.use(express.static('abcremote'));
 
-app.listen(80, function () {
+app.listen(8080, function () {
   console.log('Example app listening on port 80!');
 });
