bbdac471415c5900318c4e5060db22557ae7bfd3
Giang Nam <namle@kinssolutions.net>
2016-08-12 14:54:38 +0700

    asd

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ var app = express();
 var fs = require('fs');
 
 app.get('/', function (req, res) {
-  res.send('Hi !');
+  res.writeHead(200, {
+    'Cache-Control': 'no-cache'
+  });
+  res.end('Hi 1 !');
 });
 
 app.get('/sale', function (req, res) {
